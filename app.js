d7c3726ed4cdedfe8fb51dc4191c1684a9ad423c
Moon Patel <moonpatel2003@gmail.com>
2022-06-30 03:56:20 +0530

    1. created website 2. added home page and destination page

---
+++ app.js
@@ -0,0 +1,11 @@
+const welcomeImage = document.querySelector('div')
+
+// let i=1
+// welcomeImage.addEventListener('click',() => {
+//     if(i>5)
+//         i=1
+//     let imgName = "welcome"+i+".jpg"
+//     welcomeImage.style.backgroundImage = `url("images/${imgName}")`
+//         i++
+// })
+
