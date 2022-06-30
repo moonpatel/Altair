72136c2f75fb800993d34ea34082f590e237b1ae
Moon Patel <moonpatel2003@gmail.com>
2022-06-30 22:31:29 +0530

    project done

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const navBar = document.querySelector('nav')
+const navLink = document.querySelector('nav ul')
+
 window.onscroll = () => {
     let top = window.scrollY
-    if (top > 500)
-        navBar.style.backgroundColor = 'rgba(32, 32, 32, 20)'
+    if(top > 0) {
+        navLink.style.color = `rgb(32 ,32, 32)`
+    }
+    if (top > 500) {
+        navBar.style.backgroundColor = 'rgb(32, 32, 32)'
+    }
     else if (top > 100) {
         let opacity = ((top - 100) / 300)
         navBar.style.backgroundColor = `rgba(32, 32, 32, ${opacity})`
